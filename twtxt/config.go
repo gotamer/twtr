d6ef14b3110a1f0cdcc51d3d6b496a0b1ec2c58b
duriny <duriny@envs.net>
2021-12-21 15:58:28 +1300

    Add documentation to twtxt.Config

--- twtxt/config.go
+++ twtxt/config.go
@@ -1,5 +1,7 @@
 package twtxt
 
+// Config is a collection of configuration options that control how to post new
+// tweets, as well as retrieving the tweets from followed users.
 type Config struct {
 	nick              string
 	twtfile           string
