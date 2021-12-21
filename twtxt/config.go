7cc097bc6475ea85980f988f4bcec787d99581f7
duriny <duriny@envs.net>
2021-12-21 16:03:00 +1300

    Make field exported and types match usage not parsing

--- twtxt/config.go
+++ twtxt/config.go
@@ -1,12 +1,15 @@
 package twtxt
 
+import (
+	"net/url"
+	"os"
+)
+
 // Config is a collection of configuration options that control how to post new
 // tweets, as well as retrieving the tweets from followed users.
 type Config struct {
-	nick              string
-	twtfile           string
-	twturl            string
-	check_following   bool
-	disclose_identity bool
-	timeout           float64
+	NickName string
+	TwtFile  *os.File
+	TwtUrl   *url.URL
+	Timeout  float64
 }
