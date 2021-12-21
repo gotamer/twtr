851b5b27eade0bd4466df3899b6ce4e701ba28a3
duriny <duriny@envs.net>
2021-12-21 16:04:11 +1300

    Use embedding to include the account within the config

--- twtxt/config.go
+++ twtxt/config.go
@@ -1,15 +1,13 @@
 package twtxt
 
 import (
-	"net/url"
 	"os"
 )
 
 // Config is a collection of configuration options that control how to post new
 // tweets, as well as retrieving the tweets from followed users.
 type Config struct {
-	NickName string
-	TwtFile  *os.File
-	TwtUrl   *url.URL
-	Timeout  float64
+	Account Account
+	TwtFile *os.File
+	Timeout float64
 }
