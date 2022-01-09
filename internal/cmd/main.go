7299f748c3a56089584d274c9295ade36f33ba7d
~duriny <duriny@envs.net>
2022-01-10 10:09:11 +1300

    Fix existing tests

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -4,15 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-
-	"internal/cmd/config"
-	"internal/cmd/follow"
-	"internal/cmd/following"
-	"internal/cmd/quickstart"
-	"internal/cmd/timeline"
-	"internal/cmd/tweet"
-	"internal/cmd/unfollow"
-	"internal/cmd/view"
 )
 
 const version = "v0.0.0"
@@ -22,66 +13,26 @@ var (
 	verbose bool
 )
 
-type command struct {
-	main func(args ...string) error
-	help func(self, desc string)
-	desc string
-}
-
-var commands map[string]command = map[string]command{
-	"config": {
-		main: config.Main,
-		desc: "Update your configuration.",
-	},
-	"follow": {
-		main: follow.Main,
-		desc: "Add a new source to your followings.",
-	},
-	"following": {
-		main: following.Main,
-		desc: "View the sources that you are following.",
-	},
-	"quickstart": {
-		main: quickstart.Main,
-		desc: "Setup a new config file with the quickstart wizard.",
-	},
-	"timeline": {
-		main: timeline.Main,
-		desc: "Retrieve your personal timeline.",
-	},
-	"tweet": {
-		main: tweet.Main,
-		desc: "Send out a message into the void.",
-	},
-	"unfollow": {
-		main: unfollow.Main,
-		desc: "Remove an existing source from your list.",
-	},
-	"view": {
-		main: view.Main,
-		desc: "View a source that you follow.",
-	},
-}
-
 func help(self string) string {
 	usage := `Usage: %s COMMAND [OPTIONS] [ARGS...]
 
 Decentralized, minimalist microblogging for hackers.
 
 Options:
-    -c, --config PATH  Specify a custom config file location.
-    -v, --version      Enable verbose output for debugging.
-    --version          Show the version and exit.
-    -h, --help         Show this message and exit.
+	-c, --config PATH  Specify a custom config file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+	    --version      Show the version and exit.
 
 Commands:
-    timeline   Retrieve your personal timeline.
-    following  View the sources that you are following.
-    follow     Add a new source to your followings.
-    unfollow   Remove an existing source from your list.
-    tweet      Send out a message into the void.
-    view       View a source that you follow.
-    config     Update your configuration.
+	quickstart  Quickstart wizard for setting up twtxt.
+	timeline    Retrieve your personal timeline.
+	following   View the sources that you are following.
+	follow      Add a new source to your followings.
+	unfollow    Remove an existing source from your list.
+	tweet       Send out a message into the void.
+	view        View a source that you follow.
+	config      Update your configuration.
 `
 
 	return fmt.Sprintf(usage, self)
@@ -107,12 +58,6 @@ func Main(stdin io.Reader, stdout io.Writer, stderr io.Writer, self string, args
 	for i := 0; i < len(args); i++ {
 		arg := args[i]
 
-		if command := commands[arg]; command.main != nil {
-			if err := command.main(args[i:]...); err != nil {
-				return err
-			}
-		}
-
 		switch arg {
 		case "-c", "--config":
 			if i++; i >= len(args) {
@@ -126,7 +71,7 @@ func Main(stdin io.Reader, stdout io.Writer, stderr io.Writer, self string, args
 			fmt.Fprintln(stdout, version)
 			return nil
 		case "-h", "--help":
-			fmt.Fprintln(stderr, help(self))
+			fmt.Fprint(stderr, help(self))
 			return nil
 		default:
 			return errors.New("unknown command or flag: '" + arg + "'")
