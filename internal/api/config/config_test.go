b8ac3401fbb4a57dfa70770bd1639750532d327c
~duriny <duriny@envs.net>
2022-01-09 20:13:13 +1300

    Make test files hardcoded into test

--- internal/api/config/config_test.go
+++ internal/api/config/config_test.go
@@ -2,7 +2,7 @@ package config_test
 
 import (
 	"fmt"
-	"os"
+	"io"
 	"strings"
 	"testing"
 
@@ -13,12 +13,36 @@ import (
 
 func TestNewConfig(t *testing.T) {
 	tests := []struct {
-		file          string
+		name          string
+		source        io.Reader
 		want          config.Config
 		expectedError func(error) bool
 	}{
 		{ // All fields with example value
-			file: "_test/example1.ini",
+			name: "_test/example1.ini",
+			source: strings.NewReader(`
+[twtxt]
+nick = buckket
+twtfile = ~/twtxt.txt
+twturl = http://example.org/twtxt.txt
+check_following = True
+use_pager = False
+use_cache = True
+porcelain = False
+disclose_identity = False
+character_limit = 140
+character_warning = 140
+limit_timeline = 20
+timeline_update_interval = 10
+timeout = 5.0
+sorting = descending
+pre_tweet_hook = "scp buckket@example.org:~/public_html/twtxt.txt {twtfile}"
+post_tweet_hook = "scp {twtfile} buckket@example.org:~/public_html/twtxt.txt"
+
+[following]
+bob = https://example.org/bob.txt
+alice = https://example.org/alice.txt
+`),
 			want: config.Config{
 				Nick:                   "buckket",
 				Twtfile:                "~/twtxt.txt",
@@ -43,7 +67,26 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 		{ // No following section
-			file: "_test/example2.ini",
+			name: "_test/example2.ini",
+			source: strings.NewReader(`
+[twtxt]
+nick = buckket
+twtfile = ~/twtxt.txt
+twturl = http://example.org/twtxt.txt
+check_following = True
+use_pager = False
+use_cache = True
+porcelain = False
+disclose_identity = False
+character_limit = 140
+character_warning = 140
+limit_timeline = 20
+timeline_update_interval = 10
+timeout = 5.0
+sorting = descending
+pre_tweet_hook = "scp buckket@example.org:~/public_html/twtxt.txt {twtfile}"
+post_tweet_hook = "scp {twtfile} buckket@example.org:~/public_html/twtxt.txt"
+`),
 			want: config.Config{
 				Nick:                   "buckket",
 				Twtfile:                "~/twtxt.txt",
@@ -65,7 +108,12 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 		{ // No twtxt section (default values) + following section
-			file: "_test/example3.ini",
+			name: "_test/example3.ini",
+			source: strings.NewReader(`
+[following]
+bob = https://example.org/bob.txt
+alice = https://example.org/alice.txt
+`),
 			want: config.Config{
 				CheckFollowing:         true,
 				UseCache:               true,
@@ -79,7 +127,8 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 		{ // empty file (default values)
-			file: "_test/example4.ini",
+			name:   "_test/example4.ini",
+			source: strings.NewReader(""),
 			want: config.Config{
 				CheckFollowing:         true,
 				UseCache:               true,
@@ -90,7 +139,15 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 		{ // valid INI file but with no relevant sections (default values)
-			file: "_test/example5.ini",
+			name: "_test/example5.ini",
+			source: strings.NewReader(`
+[Section A]
+magic = on
+deepMagic = off
+
+[Section B]
+samuraiWarrior = "foolish"
+`),
 			want: config.Config{
 				CheckFollowing:         true,
 				UseCache:               true,
@@ -101,7 +158,16 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 		{ // valid INI file with relevant sections, but no real values (default values)
-			file: "_test/example6.ini",
+			name: "_test/example6.ini",
+			source: strings.NewReader(`
+[twtxt]
+magic = enable
+deepMagic = DEAR_GOD_TURN_IT_OFF
+
+[following]
+notActuallyANickname = "Not actually a url"
+meaningOfLife = 42
+`),
 			want: config.Config{
 				CheckFollowing:         true,
 				UseCache:               true,
@@ -115,13 +181,79 @@ func TestNewConfig(t *testing.T) {
 			},
 		},
 		{ // invalid INI file (parse error)
-			file: "_test/example7.ini",
+			name: "_test/example7.ini",
+			source: strings.NewReader(`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum nisi
+ac nisi vulputate, efficitur varius risus ultricies. Sed fringilla hendrerit
+augue, a porta nunc placerat a. Donec ac nisi scelerisque, gravida diam iaculis,
+molestie libero. Nam congue tristique ipsum, nec interdum metus maximus id.
+Aenean nec sodales lectus, et eleifend nibh. Nulla sit amet pellentesque elit.
+Vestibulum ultricies porta nibh eu convallis.
+
+Aliquam ut dui vitae nisi faucibus fermentum faucibus vel tellus. Proin aliquam
+nisi quis metus ornare, ullamcorper ultrices erat euismod. Nulla semper
+consequat ipsum eget sagittis. Vestibulum egestas pellentesque nisl, eget
+gravida magna finibus sit amet. Morbi tristique ac magna at rutrum. Nullam
+efficitur sollicitudin vestibulum. Nullam non justo massa. Vivamus maximus, enim
+in sodales euismod, turpis nunc egestas arcu, nec bibendum augue dolor ut augue.
+Donec malesuada ut arcu sed mattis. Nulla consectetur felis at varius finibus.
+Vivamus porta nibh justo, eu ultrices ante ullamcorper eget.
+
+Phasellus maximus magna sed neque viverra, maximus rhoncus arcu accumsan. Nam
+convallis metus id nunc aliquet, sed placerat ante placerat. Maecenas quis
+sapien hendrerit, mattis turpis ut, convallis mauris. Lorem ipsum dolor sit
+amet, consectetur adipiscing elit. Suspendisse mattis ultricies elit, commodo
+lacinia leo. Aenean at dolor risus. Donec vel imperdiet ante. Praesent pharetra
+ex tincidunt sapien interdum, ac accumsan lacus blandit. Donec risus augue,
+ornare sed elit sed, porta varius ex. Proin sit amet eros at neque condimentum
+laoreet ut vitae nisi.
+
+In dignissim, sem sed viverra gravida, velit turpis sagittis lacus, non vehicula
+magna mi quis tellus. Vestibulum ullamcorper justo eu est lacinia, id porttitor
+ipsum tempor. Orci varius natoque penatibus et magnis dis parturient montes,
+nascetur ridiculus mus. Pellentesque tristique nisi lacus, ut cursus dui
+accumsan vel. Cras semper commodo magna, eu blandit mauris euismod id. Donec
+justo mi, tempor sed tellus facilisis, lacinia imperdiet eros. Etiam in placerat
+erat. Pellentesque nibh nibh, commodo a dolor quis, tempor efficitur orci.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed luctus auctor
+velit, in euismod magna interdum rutrum. Etiam sit amet mi et urna elementum
+condimentum. Proin pellentesque lectus ac malesuada mollis. Nulla ornare
+dignissim felis, vel lobortis dui malesuada sed. Sed tempor luctus libero.
+Aenean convallis nec enim placerat lobortis. Praesent eleifend nisl tortor, a
+imperdiet urna dictum in. Cras pellentesque eleifend sapien at elementum. Proin
+aliquam placerat quam. Cras vel vestibulum mauris, nec viverra purus. Curabitur
+dolor neque, commodo sed luctus ac, lacinia non mi.
+`),
 			expectedError: func(err error) bool {
 				return ini.IsErrDelimiterNotFound(err)
 			},
 		},
 		{ // valid INI file but wrong types for values (parse error)
-			file: "_test/example8.ini",
+			name: "_test/example8.ini",
+			source: strings.NewReader(`
+[twtxt]
+nick = buckket
+twtfile = ~/twtxt.txt
+twturl = http://example.org/twtxt.txt
+check_following = True
+use_pager = "not a boolean string"
+use_cache = "not a boolean string"
+porcelain = False
+disclose_identity = False
+character_limit = 140.123
+character_warning = 140.123
+limit_timeline = 20
+timeline_update_interval = 10
+timeout = 5.0
+sorting = neither_descending_nor_ascending
+pre_tweet_hook = "scp buckket@example.org:~/public_html/twtxt.txt {twtfile}"
+post_tweet_hook = "scp {twtfile} buckket@example.org:~/public_html/twtxt.txt"
+
+[following]
+bob = https://example.org/bob.txt
+alice = https://example.org/alice.txt
+`),
 			expectedError: func(err error) bool {
 				return strings.Contains(err.Error(), "invalid syntax")
 			},
@@ -130,21 +262,14 @@ func TestNewConfig(t *testing.T) {
 
 	for _, test := range tests {
 		test := test
-		name := test.file
-		want := test.want
 
-		t.Run(name, func(t *testing.T) {
-			file, err := os.Open(test.file)
-			defer file.Close()
-			if err != nil {
-				t.Fatalf("could not open file '%s': %q", test.file, err)
-			}
+		t.Run(test.name, func(t *testing.T) {
+			want := test.want
 
-			have, err := config.New(file)
+			have, err := config.New(test.source)
 			if err != nil {
 				if test.expectedError != nil && test.expectedError(err) {
-					t.Logf("expected error: %q", err)
-					t.Skip()
+					t.Skipf("expected error: %q", err)
 				} else {
 					t.Fatalf("unexpected error: %q", err)
 				}
