7d9d3929f91c12c92911b19616e6b429289d0f27
~duriny <duriny@envs.net>
2022-01-10 09:34:09 +1300

    Add all help message tests

--- internal/cmd/main_test.go
+++ internal/cmd/main_test.go
@@ -8,25 +8,142 @@ import (
 	"internal/cmd"
 )
 
-const help = `Usage: twtxt COMMAND [OPTIONS] [ARGS...]
+const (
+	sourceUsage = `Sources:
+	At least one SOURCE must be given (unless called with -h), each SOURCE
+	consists of a NICK and a URL. Allowed formats are NICK@URL or NICK URL, if
+	you don't know the nickname of a SOURCE, you can make one up, or use the
+	domain part of the URL (this can be easily changed later).
+`
+
+	help = `Usage: twtr COMMAND [OPTIONS] [ARGS...]
 
 Decentralized, minimalist microblogging for hackers.
 
 Options:
-    -c, --config PATH  Specify a custom config file location.
-    -v, --version      Enable verbose output for debugging.
-    --version          Show the version and exit.
-    -h, --help         Show this message and exit.
+	-c, --config PATH  Specify a custom config file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+	    --version      Show the version and exit.
 
 Commands:
-    timeline   Retrieve your personal timeline.
-    following  View the sources that you are following.
-    follow     Add a new source to your followings.
-    unfollow   Remove an existing source from your list.
-    tweet      Send out a message into the void.
-    view       View a source that you follow.
-    config     Update your configuration.
+	quickstart  Quickstart wizard for setting up twtxt.
+	timeline    Retrieve your personal timeline.
+	following   View the sources that you are following.
+	follow      Add a new source to your followings.
+	unfollow    Remove an existing source from your list.
+	tweet       Send out a message into the void.
+	view        View a source that you follow.
+	config      Update your configuration.
 `
+
+	quickstartUsage = `Usage: twtr quickstart [-cfhnuv] [--disclose-identity] [--follow-news]
+
+Quickstart wizard for setting up twtxt.
+
+Options:
+	-c, --config PATH        Specify a custom configuration file location.
+	    --disclose-identity  Show your nickname and url in the User Agent.
+	-f, --file PATH          Specify a custom twtxt file location.
+	    --follow-news        Follow the official twtxt and twtr news feeds.
+	-h, --help               Show this message and exit.
+	-n, --nick NICK          Specify the nickname for your feed.
+	-u, --url URL            Specify the url that your feed will be hosted at.
+	-v, --verbose            Enable verbose output for debugging.
+`
+
+	timelineUsage = `Usage: twtr timeline [-chv] [--limit COUNT] [--sort ascending | descending]
+
+Retrieve your personal timeline.
+
+Options:
+	-c, --config PATH     Specify a custom configuration file location.
+	-h, --help            Show this message and exit.
+	    --limit COUNT     Limit the amount of tweets shown.
+	    --sort DIRECTION  Sort tweets ascending or descending by timestamp.
+	-v, --verbose         Enable verbose output for debugging.
+`
+
+	followingUsage = `Usage: twtr following [-chv]
+
+View the sources that you are following.
+
+Options:
+	-c, --config PATH  Specify a custom configuration file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+`
+
+	followUsage = `Usage: twtr follow [-chv] [--replace] SOURCE [SOURCES...]
+
+Add a new source to your following.
+
+Options:
+	-c, --config PATH  Specify a custom configuration file location.
+	-h, --help         Show this message and exit.
+	    --replace      Replace duplicates instead of returning error.
+	-v, --verbose      Enable verbose output for debugging.
+
+` + sourceUsage
+
+	unfollowUsage = `Usage: twtr unfollow [-chv] NICK|URL
+
+Remove an existing source from your list.
+
+Options:
+	-c, --config PATH  Specify a custom configuration file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+`
+
+	tweetUsage = `Usage: twtr tweet [-cfhv] TWEET
+
+Send out a message into the void.
+
+Options:
+	-c, --config PATH  Specify a custom configuration file location.
+	-f, --file PATH    Specify a custom twtxt file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+`
+
+	viewUsage = `Usage: twtr view [-chv] SOURCE [SOURCES]
+
+View a source that you follow.
+
+Options:
+	-c, --config PATH  Specify a custom configuration file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+
+` + sourceUsage
+
+	configUsage = `Usage: twtr config [-chv] [--edit] [--remove KEY]|[KEY [VALUE]]
+
+Update your configuration.
+
+Options:
+	-c, --config PATH  Specify a custom configuration file location.
+	    --edit         Edit the configuration file manually.
+	-h, --help         Show this message and exit.
+	   --remove KEY    Remove a configuration by its KEY, e.g. twtxt.nick.
+	-v, --verbose      Enable verbose output for debugging.
+
+Examples:
+	Get config value:
+		$ twtr config twtxt.nick
+		> YourOldNickName
+
+	Set config value:
+		$ twtr config twtxt.nick YourNewNickName
+
+	Remove config value:
+		$ twtr config --remove twtxt.nick
+
+	Edit config file manually:
+		$ twtr config --edit
+`
+)
 
 func TestMain(t *testing.T) {
 	tests := []struct {
@@ -50,6 +167,78 @@ func TestMain(t *testing.T) {
 		{
 			args:   []string{"--help"},
 			stderr: help,
+		},
+		{
+			args:   []string{"quickstart", "-h"},
+			stderr: quickstartUsage,
+		},
+		{
+			args:   []string{"quickstart", "--help"},
+			stderr: followingUsage,
+		},
+		{
+			args:   []string{"timeline", "-h"},
+			stderr: followingUsage,
+		},
+		{
+			args:   []string{"timeline", "--help"},
+			stderr: followingUsage,
+		},
+		{
+			args:   []string{"following", "-h"},
+			stderr: followingUsage,
+		},
+		{
+			args:   []string{"following", "--help"},
+			stderr: followingUsage,
+		},
+		{
+			args:   []string{"follow"},
+			stderr: followUsage,
+		},
+		{
+			args:   []string{"follow", "-h"},
+			stderr: followUsage,
+		},
+		{
+			args:   []string{"follow", "--help"},
+			stderr: followUsage,
+		},
+		{
+			args:   []string{"unfollow"},
+			stderr: unfollowUsage,
+		},
+		{
+			args:   []string{"unfollow", "-h"},
+			stderr: unfollowUsage,
+		},
+		{
+			args:   []string{"unfollow", "--help"},
+			stderr: unfollowUsage,
+		},
+		{
+			args:   []string{"tweet"},
+			stderr: tweetUsage,
+		},
+		{
+			args:   []string{"tweet", "-h"},
+			stderr: tweetUsage,
+		},
+		{
+			args:   []string{"tweet", "--help"},
+			stderr: tweetUsage,
+		},
+		{
+			args:   []string{"view"},
+			stderr: viewUsage,
+		},
+		{
+			args:   []string{"view", "-h"},
+			stderr: viewUsage,
+		},
+		{
+			args:   []string{"view", "--help"},
+			stderr: viewUsage,
 		},
 	}
 
