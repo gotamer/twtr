61f1e59c7063d0bf909fd8548d2d13bda90a0004
~duriny <duriny@envs.net>
2022-01-10 17:15:18 +1300

    Move help definition to main, remove dead code

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -8,6 +8,31 @@ import (
 
 const version = "v0.0.0"
 
+func help(ctx *Context) string {
+	usage := `Usage: %s COMMAND [OPTIONS] [ARGS...]
+
+Decentralized, minimalist microblogging for hackers.
+
+Options:
+	-c, --config PATH  Specify a custom config file location.
+	-h, --help         Show this message and exit.
+	-v, --verbose      Enable verbose output for debugging.
+	    --version      Show the version and exit.
+
+Commands:
+	quickstart  Quickstart wizard for setting up twtxt.
+	timeline    Retrieve your personal timeline.
+	following   View the sources that you are following.
+	follow      Add a new source to your followings.
+	unfollow    Remove an existing source from your list.
+	tweet       Send out a message into the void.
+	view        View a source that you follow.
+	config      Update your configuration.
+`
+
+	return fmt.Sprintf(usage, ctx.Self)
+}
+
 func Main(ctx *Context, args ...string) error {
 	if ctx == nil {
 		ctx = &Context{}
