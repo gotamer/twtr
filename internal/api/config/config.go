fe4c7cfff8ae6a05c2c408e3e6b438b2aed0a78a
~duriny <duriny@envs.net>
2022-01-12 17:29:11 +1300

    Implement simple Save() method

--- internal/api/config/config.go
+++ internal/api/config/config.go
@@ -175,6 +175,110 @@ func New(source io.Reader) (*Config, error) {
 
 // Save writes an existing config to the given writer, allowing the config to be
 // saved to a file.
-func (c *Config) Save(to io.Writer) error {
-	return nil
+func (c *Config) Save(to io.Writer) (err error) {
+	var twtxt *ini.Section
+
+	file := ini.Empty()
+	fmti := func(i int) string { return fmt.Sprintf("%d", i) }
+	fmtf := func(f float64) string { return fmt.Sprintf("%f", f) }
+	fmtt := func(b bool, t string, f string) string {
+		if b {
+			return t
+		} else {
+			return f
+		}
+	}
+	fmtb := func(b bool) string { return fmtt(b, "True", "False") }
+
+	if twtxt, err = file.NewSection("twtxt"); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("nick", c.Nick); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("twtxt", c.Twtfile); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("twturl", c.Twturl); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("check_following", fmtb(c.CheckFollowing)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("use_pager", fmtb(c.UsePager)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("use_cache", fmtb(c.UseCache)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("porcelain", fmtb(c.Porcelain)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("disclose_identity", fmtb(c.DiscloseIdentity)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("character_limit", fmti(c.CharacterLimit)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("character_warning", fmti(c.CharacterWarning)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("limit_timeline", fmti(c.LimitTimeline)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("timeline_update_interval", fmti(c.TimelineUpdateInterval)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("timeout", fmtf(c.Timeout)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("sorting", fmtt(c.SortAscending, "ascending", "descending")); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("use_abs_time", fmtb(c.UseAbsoluteTime)); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("pre_tweet_hook", c.PreTweetHook); err != nil {
+		return
+	}
+
+	if _, err = twtxt.NewKey("post_tweet_hook", c.PostTweetHook); err != nil {
+		return
+	}
+
+	if len(c.Following) > 0 {
+		var following *ini.Section
+
+		if following, err = file.NewSection("following"); err != nil {
+			return
+		}
+
+		for nick, url := range c.Following {
+			if _, err = following.NewKey(nick, url); err != nil {
+				return
+			}
+		}
+	}
+
+	if _, err = file.WriteTo(to); err != nil {
+		return
+	}
+
+	return
 }
