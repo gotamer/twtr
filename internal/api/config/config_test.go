9e6a681bdace70749dc307596375c84cfab3f5f1
~duriny <duriny@envs.net>
2022-01-13 11:52:44 +1300

    Test that the save method is reversable

--- internal/api/config/config_test.go
+++ internal/api/config/config_test.go
@@ -435,6 +435,87 @@ bob   = https://example.org/bob.txt
 					t.Errorf("line %d:\nhave %q\nwant %q\n", i, have, want)
 				}
 			}
+
+			t.Run("Reversibly", func(t *testing.T) {
+				want := test.from
+
+				have, err := config.New(strings.NewReader(test.want))
+				if err != nil {
+					t.Fatalf("unexpected error: %q", err)
+				}
+
+				if have.Nick != want.Nick {
+					t.Errorf("Nick = %q, want %q", have.Nick, want.Nick)
+				}
+
+				if have.Twtfile != want.Twtfile {
+					t.Errorf("Twtfile = %q, want %q", have.Twtfile, want.Twtfile)
+				}
+
+				if have.Twturl != want.Twturl {
+					t.Errorf("Twturl = %q, want %q", have.Twturl, want.Twturl)
+				}
+
+				if have.CheckFollowing != want.CheckFollowing {
+					t.Errorf("CheckFollowing = %t, want %t", have.CheckFollowing, want.CheckFollowing)
+				}
+
+				if have.UsePager != want.UsePager {
+					t.Errorf("UsePager = %t, want %t", have.UsePager, want.UsePager)
+				}
+
+				if have.UseCache != want.UseCache {
+					t.Errorf("UseCache = %t, want %t", have.UseCache, want.UseCache)
+				}
+
+				if have.Porcelain != want.Porcelain {
+					t.Errorf("Porcelain = %t, want %t", have.Porcelain, want.Porcelain)
+				}
+
+				if have.DiscloseIdentity != want.DiscloseIdentity {
+					t.Errorf("DiscloseIdentity = %t, want %t", have.DiscloseIdentity, want.DiscloseIdentity)
+				}
+
+				if have.CharacterLimit != want.CharacterLimit {
+					t.Errorf("CharacterLimit = %q, want %q", have.CharacterLimit, want.CharacterLimit)
+				}
+
+				if have.CharacterWarning != want.CharacterWarning {
+					t.Errorf("CharacterWarning = %q, want %q", have.CharacterWarning, want.CharacterWarning)
+				}
+
+				if have.LimitTimeline != want.LimitTimeline {
+					t.Errorf("LimitTimeline = %q, want %q", have.LimitTimeline, want.LimitTimeline)
+				}
+
+				if have.TimelineUpdateInterval != want.TimelineUpdateInterval {
+					t.Errorf("TimelineUpdateInterval = %q, want %q", have.TimelineUpdateInterval, want.TimelineUpdateInterval)
+				}
+
+				if have.Timeout != want.Timeout {
+					t.Errorf("Timeout = %f, want %f", have.Timeout, want.Timeout)
+				}
+
+				if have.SortAscending != want.SortAscending {
+					t.Errorf("SortAscending = %t, want %t", have.SortAscending, want.SortAscending)
+				}
+
+				if have.UseAbsoluteTime != want.UseAbsoluteTime {
+					t.Errorf("UseAbsoluteTime = %t, want %t", have.UseAbsoluteTime, want.UseAbsoluteTime)
+				}
+
+				if have.PreTweetHook != want.PreTweetHook {
+					t.Errorf("PreTweetHook = %q, want %q", have.PreTweetHook, want.PreTweetHook)
+				}
+
+				if have.PostTweetHook != want.PostTweetHook {
+					t.Errorf("PostTweetHook = %q, want %q", have.PostTweetHook, want.PostTweetHook)
+				}
+
+				if fmt.Sprint(have.Following) != fmt.Sprint(want.Following) {
+					t.Errorf("Following = %#v, want %#v", have.Following, want.Following)
+				}
+			})
 		})
 	}
 }
